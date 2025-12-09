506ba606d608ca29166fe901244cb6958d838608
Haitao <wanghaitao@jingbenzhengyi.com>
2025-12-09 16:27:03 +0800

    Initial Commit

---
+++ AutoTyper/AutoTyperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTyperApp.swift
+//  AutoTyper
+//
+//  Created by Hector on 9/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct AutoTyperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoTyper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoTyper
+//
+//  Created by Hector on 9/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
